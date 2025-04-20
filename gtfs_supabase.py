48b1d98a395deed5595e22ebc1ef1f1a744c2a4b
jozwang <9334262+jozwang@users.noreply.github.com>
2025-04-20 16:14:33 +1000

    Update gtfs_supabase.py

--- gtfs_supabase.py
+++ gtfs_supabase.py
@@ -13,7 +13,7 @@ PG_HOST = "eegejlqdgahlmtjniupz.supabase.co"
 PG_PORT = 5432
 PG_DB = "postgres"
 PG_USER = "postgres"
-PG_PASSWORD = "Supa1base!"  # Enter your actual DB password here
+PG_PASSWORD = "Supa1base!gtfs"  # Enter your actual DB password here
 
 # --- GTFS Static Data URL ---
 GTFS_ZIP_URL = "https://www.data.qld.gov.au/dataset/general-transit-feed-specification-gtfs-translink/resource/e43b6b9f-fc2b-4630-a7c9-86dd5483552b/download"
